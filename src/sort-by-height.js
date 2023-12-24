90287e347e4b105b5ef0fd705806fa0a0f9bd7e4
Yamshchikova Evgeniya <31244230+Kinkandju@users.noreply.github.com>
2023-12-24 13:28:33 +0300

    feat: add function sortByHeight

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -10,10 +10,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
- */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+*/
+
+function sortByHeight(arr) {
+  const sortedArray = [];
+
+  // iterate through the entire array, take all values that 
+  // are not equal to -1 and add them to the sorted array
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] !== -1) {
+      sortedArray.push(arr[i]);
+    }
+  }
+
+  // sorting the new array in ascending order of values
+  sortedArray.sort((a, b) => a - b);
+
+  const resultingArray = [];
+  let currentIndex = 0;
+
+  // iterate through the entire array
+  for (let i = 0; i < arr.length; i += 1) {
+
+    // if the value is -1, then add it to the resulting array
+    if (arr[i] === -1) {
+      resultingArray.push(-1);
+    } 
+    
+    // if the value is not -1, then add the current value 
+    // from the sorted array to the resulting array
+    else {
+      resultingArray.push(sortedArray[currentIndex]);
+
+      // increasing index to move to the next value
+      currentIndex++;
+    }
+  }
+
+  return resultingArray;
 }
 
 module.exports = {
