8e35daf7d698813b0970d54e1557f85cc7ba8925
Yamshchikova Evgeniya <31244230+Kinkandju@users.noreply.github.com>
2023-12-24 13:51:39 +0300

    refactor: add comments to function encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -9,25 +9,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * For aabbbc should return 2a3bc
  *
- */
+*/
+
 function encodeLine(str) {
-  let encodeStr = '';
+  let encodeString = '';
   let counter = 1;
 
+  // iterate through the entire string
   for (let i = 0; i < str.length; i += 1) {
+
+    // check if the current symbol is the same as the next symbol
     if (str[i] === str[i + 1]) {
       counter += 1;
-    } else {
+    } 
+    
+    // if the next symbol is different
+    else {
+
+      // If the number of repetitions is not equal to 1
       if (counter !== 1) {
-        encodeStr += counter + str[i];
-      } else {
-        encodeStr += str[i];
+
+        // add the count and current symbol to the encoded string
+        encodeString += counter + str[i];
+      } 
+      
+      // otherwise, add only the current symbol
+      else {
+        encodeString += str[i];
       }
+
+      // reset the counter
       counter = 1;
     }
   }
 
-  return encodeStr;
+  return encodeString;
 }
 
 module.exports = {
