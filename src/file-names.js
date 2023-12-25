22810498976b1190e027e86f0fec4722fbf6cfcb
Yamshchikova Evgeniya <31244230+Kinkandju@users.noreply.github.com>
2023-12-25 18:10:51 +0300

    feat: add function renameFiles

--- src/file-names.js
+++ src/file-names.js
@@ -14,10 +14,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For input ["file", "file", "image", "file(1)", "file"],
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
- */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+*/
+
+function renameFiles(names) {
+  let counts = {};
+
+  /*
+  let counts = {
+    "file": 3,
+    "image": 1,
+    "file(1)": 1,
+    "file(2)": 1
+  };
+  */
+
+  for (let i = 0; i < names.length; i += 1) {
+    let name = names[i];
+    
+    // if the name already exists, append 'k' to it
+    if (counts[name]) {
+      let newName = name + '(' + counts[name] + ')';
+
+      // increment count for the updated name
+      counts[newName] = (counts[newName] || 0) + 1;
+      names[i] = newName;
+    }
+    
+    // increment count for the original name
+    counts[name] = (counts[name] || 0) + 1;
+  }
+
+  return names;
 }
 
 module.exports = {
