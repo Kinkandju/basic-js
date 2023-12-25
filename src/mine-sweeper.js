e552f471ee05998c8f608d3b25feb9131c8f89fc
Yamshchikova Evgeniya <31244230+Kinkandju@users.noreply.github.com>
2023-12-25 18:54:06 +0300

    feat: function minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -22,10 +22,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [2, 1, 1],
  *  [1, 1, 1]
  * ]
- */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+*/
+
+function minesweeper(matrix) {
+  // create a new matrix to store the result
+  const result = [];
+
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  // get the number of rows and columns for iterations over the input matrix
+  for (let i = 0; i < rows; i += 1) {
+    result[i] = [];
+    for (let j = 0; j < cols; j += 1) {
+      let count = 0;
+
+      // initialize a count variable for the current cell
+      // check each neighboring cell (including diagonals)
+      for (let x = -1; x <= 1; x += 1) {
+        for (let y = -1; y <= 1; y += 1) {
+          const neighborRow = i + x;
+          const neighborCol = j + y;
+
+          // skip the current cell or neighbor if they don't exist
+          if (x === 0 && y === 0) continue;
+          if (neighborRow < 0 || neighborCol < 0) continue;
+          if (neighborRow >= rows || neighborCol >= cols) continue;
+
+          // if the neighboring cell contains a mine (true), increment the count
+          if (matrix[neighborRow][neighborCol]) {
+            count += 1;
+          }
+        }
+      }
+
+      // update the corresponding cell in the result matrix with the count of mines
+      result[i][j] = count;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
