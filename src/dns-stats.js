94f9d4b7c2a00108a0df65301b3fde5e21f81800
Yamshchikova Evgeniya <31244230+Kinkandju@users.noreply.github.com>
2023-12-25 18:30:54 +0300

    feat: add function getDNSStats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -21,10 +21,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   '.ru.yandex.music': 1,
  * }
  *
- */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+*/
+
+function getDNSStats(domains) {
+  // object to store the appearances of the DNS
+  const appearances = {};
+
+  for (const domain of domains) {
+
+    // split the domain into subdomains
+    const subdomains = domain.split('.');
+
+    // variable to keep track of the current domain level
+    let currentLevel = '';
+
+    // build the current domain level by adding subdomains in reverse order
+    for (let i = subdomains.length - 1; i >= 0; i -= 1) {
+      currentLevel = subdomains[i] + (currentLevel ? '.' + currentLevel : '');
+
+      // if the current domain level exists as a property in the appearances object, increment its count by 1;
+      // otherwise, initialize it with a count of 1
+      appearances[currentLevel] = appearances[currentLevel] ? appearances[currentLevel] + 1 : 1;
+    }
+  }
+
+  // new appearances object with the correct hierarchy of DNS
+  const result = {};
+
+  for (const key in appearances) {
+    result[`.${key.split('.').reverse().join('.')}`] = appearances[key];
+  }
+
+  return result;
 }
 
 module.exports = {
