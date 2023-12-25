404bf1275d130eb08a565c1d05045a24e4642b2b
Yamshchikova Evgeniya <31244230+Kinkandju@users.noreply.github.com>
2023-12-25 13:52:40 +0300

    feat: add function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -14,10 +14,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
- */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+*/
+
+function repeater(str, options) {
+  // number of repetitions of the str
+  let repeatTimes = options.repeatTimes || 1;
+
+  // string separating repetitions of the str
+  let separator = options.separator || '+';
+
+  // additional string that will be added to each repetition of the str
+  let addition = (options.addition !== undefined) ? String(options.addition) : '';
+
+  // number of repetitions of the addition
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+
+  // string separating repetitions of the addition
+  let additionSeparator = options.additionSeparator || '|';
+
+  let repeatedAddition = (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+  let repeatedStr = (str + repeatedAddition + separator).repeat(repeatTimes - 1) + str + repeatedAddition;
+
+  return repeatedStr;
 }
 
 module.exports = {
